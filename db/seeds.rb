3b1868858c030a206c65acc7fb0b06f330b342b8
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-06 15:30:44 -0500

    Assignment 16 - Seeding Data: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,13 @@ posts = Post.all
    body: RandomData.random_paragraph
  )
 end
+comments = Comment.all
+
+# If Post doesn't exist, this creates it
+posts.find_or_create_by(id: 51, title: "A Unique Post", body: "A Unique Post Body")
+
+# If Post Comment doesn't exist, this creates it
+comments.find_or_create_by(post_id: 51, body: "A Unique Post Comment")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
