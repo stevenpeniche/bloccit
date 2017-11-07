49a19d6da6a4e3023d9158c1e042f16c39828724
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-07 10:53:20 -0500

    Assignment 17 - CRUD: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
 		@posts = Post.all
+
+		# Filters out sensitive title for every fifth Post
+		@posts.each_with_index do |post, index|
+			post.update!(title: "SPAM") if index == 0 || (index + 1) % 5 == 0
+		end
   end
 
   def show
