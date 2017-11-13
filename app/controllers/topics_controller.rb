9eeb9a3169aac41f45ccd3d36c0c60b8c66effe7
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-13 16:02:37 -0500

    Checkpoint 26 - Posts & Users: complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,10 +12,7 @@ class TopicsController < ApplicationController
 	end
 
 	def create
-		@topic = Topic.new
-		@topic.name = params[:topic][:name]
-		@topic.description = params[:topic][:description]
-		@topic.public = params[:topic][:public]
+		@topic = Topic.new(topic_params)
 
 		if @topic.save
 			redirect_to @topic, notice: "Topic was saved successfully."
@@ -32,13 +29,10 @@ class TopicsController < ApplicationController
 
 	def update
 		@topic = Topic.find(params[:id])
-
-		@topic.name = params[:topic][:name]
-		@topic.description = params[:topic][:description]
-		@topic.public = params[:topic][:public]
+		@topic.assign_attributes(topic_params)
 
 		if @topic.save
-			 flash[:notice] = "Topic was updated."
+			flash[:notice] = "Topic was updated."
 			redirect_to @topic
 		else
 			flash.now[:alert] = "Error saving topic. Please try again."
@@ -47,14 +41,19 @@ class TopicsController < ApplicationController
 	end
 
 	def destroy
-	 @topic = Topic.find(params[:id])
+		@topic = Topic.find(params[:id])
 
-	 if @topic.destroy
-	   flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-	   redirect_to action: :index
-	 else
-	   flash.now[:alert] = "There was an error deleting the topic."
-	   render :show
-	 end
+		if @topic.destroy
+			flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+			redirect_to action: :index
+		else
+			flash.now[:alert] = "There was an error deleting the topic."
+			render :show
+		end
+	end
+
+	private
+	def topic_params
+		params.require(:topic).permit(:name, :description, :public)
 	end
 end
