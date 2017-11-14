29ebc494263a70090ebf3a384130c76b5b48ab8b
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-14 12:21:23 -0500

    Assignemnt 29 - Voting: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,7 +61,7 @@ class PostsController < ApplicationController
 
 	def authorize_user
 		post = Post.find(params[:id])
-		
+
 		unless current_user == post.user || current_user.admin?
 			flash[:alert] = "You must be an admin to do that."
 			redirect_to [post.topic, post]
