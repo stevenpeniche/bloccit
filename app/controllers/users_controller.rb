73412d0e2145a38d220bc03cf44761f0486be881
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-15 11:40:14 -0500

    Assignment 31 - Factories: complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,5 +23,11 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@posts = @user.posts.visible_to(current_user)
+		@favorites = @user.favorites
+
+		@favorites_post_array = []
+		@favorites.each do |favorite|
+			@favorites_post_array << Post.find(favorite.post_id)
+		end
 	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+	def user_has_any_post?
+		current_user.posts.count > 0
+	end
+
+	def user_has_any_comments?
+		current_user.comments.count > 0
+	end
+
+	def user_has_any_favorites?
+		current_user.favorites.count > 0
+	end
 end
